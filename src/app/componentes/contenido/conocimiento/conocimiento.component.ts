bfd9e807f42901909d074da0503338936b1ba526
mat286 <riveramateo10432@gmail.com>
2023-04-13 03:47:59 +0200

    5.0

--- src/app/componentes/contenido/conocimiento/conocimiento.component.ts
+++ src/app/componentes/contenido/conocimiento/conocimiento.component.ts
@@ -9,30 +9,48 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./conocimiento.component.css']
 })
 export class ConocimientoComponent {
-  conocimiento:any;
+  conocimiento: any;
   form: FormGroup;
 
-  miPorfolio:any;
+  miPorfolio: any;
+
+  herramientas: boolean= true;
+  lenguajeProgrmacion: boolean= true;
+  habilidadesBlandas: boolean= false;
+  Lenguajes: boolean= false;
   constructor(private datosPorfolio: PorfolioService, private formBuilder: FormBuilder) {
 
-    this.form = this.formBuilder.group({ 
-      habilidadesBlandas: ['', ],
+    this.form = this.formBuilder.group({
+      habilidadesBlandas: ['',],
       apellido: ['', [Validators.required]]
-    });
-
-   }
-
-  ngOnInit(): void {
-
-    this.datosPorfolio.obtenerdatos().subscribe(data=>{ 
-      console.log(data);
-      this.miPorfolio=data.contenido.conocimiento;
-      this.conocimiento=data.contenido.conocimiento.conocimiento; 
     });
 
   }
 
-  comparador():void{
+  ngOnInit(): void {
+
+    this.datosPorfolio.obtenerdatos().subscribe(data => {
+      this.miPorfolio = data.contenido.conocimiento;
+      this.conocimiento = data.contenido.conocimiento.conocimiento;
+    });
+
+  }
+
+  filtro(): any[] {
+    let lista: any[]=[];
+    for (let i = 0; i < this.conocimiento.length; i++) {
+      const element = this.conocimiento[i];
+      if (element.saber==="lenguajeProgrmacion" && this.lenguajeProgrmacion || element.saber==="herramientas" && this.herramientas|| element.saber==="habilidadesBlandas" && this.habilidadesBlandas || element.saber==="Lenguajes" && this.Lenguajes) {
+        lista.push(element);
+      }
+      
+    }
+    /* console.log(lista) */
+    return lista;
+  }
+
+
+  comparador(): void {
     alert(this.form.value.habilidadesBlandas.checked)
   }
 
